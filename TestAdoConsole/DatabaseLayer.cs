8b9f67eeb973b641f2eccd93185aa168acbdc7e4
S. Irfan Ali <syedirfanali@outlook.com>
2022-01-02 19:36:50 -0800

    Add project files.

---
+++ TestAdoConsole/DatabaseLayer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAdoConsole
+{
+    public class DatabaseLayer
+    {
+        public static string connstr = "Server=(localdb)\\mssqllocaldb;Database=aspnet-TestApp-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        public static void PrintList()
+        {
+            using (SqlConnection connection = new SqlConnection(connstr))
+            {
+                var command = $"Select * from PersonalDetails";
+                using (SqlCommand cmd = new(command,connection))
+                {
+                    connection.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        var id = rdr["Id"].ToString();
+                        var name = rdr["Name"].ToString();
+                        var mobile = rdr["Mobile"].ToString();
+                        Console.WriteLine($"Id: {id} & Name: {name} & mobile: {mobile}");
+                    }
+                    connection.Close();
+                }
+            }
+        }
+        public static void PrintSingle(int Id=1)
+        {
+            if (Id > 0)
+            {
+                using (SqlConnection conn = new SqlConnection(connstr))
+                {
+                    var command = $"Select * from PersonalDetails where Id = {Id}";
+                    using (SqlCommand cmd = new SqlCommand(command,conn))
+                    {
+                        conn.Open();
+                        var reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            var id = reader["Id"].ToString();
+                            var name = reader["Name"].ToString();
+                            var mobile = reader["Mobile"].ToString();
+                            Console.WriteLine($"Id:{id} | Name:{name} | Moble: {mobile}");
+                        }
+                        conn.Close();
+                    }
+                }
+            }
+        }
+        public static void EditSingle(int Id = 1, string Name="no name", string Mobile="no mobile")
+        {
+            var command = $"Update PersonalDetails Set Name = '{Name}', Mobile = '{Mobile}' where Id = {Id}";
+            using (SqlConnection conn = new SqlConnection(connstr))
+            {
+                using (SqlCommand cmd = new SqlCommand(command,conn))
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+        public static void InsertSingle(string Name, string Mobile)
+        {
+            var command = $"Insert into PersonalDetails (Name,Mobile) values ('{Name}','{Mobile}')";
+            using (SqlConnection conn = new SqlConnection(connstr))
+            {
+                using(SqlCommand cmd = new SqlCommand(command, conn))
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+        public static void DeleteSingle(int Id)
+        {
+            var command = $"Delete from PersonalDetails where Id = {Id}";
+            using(SqlConnection conn=new SqlConnection(connstr))
+            {
+                using(SqlCommand cmd = new SqlCommand(command, conn))
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ TestAdoConsole/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace TestAdoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DatabaseLayer.EditSingle(4,"updated name","00000000");
+            //DatabaseLayer.InsertSingle("another name", "33242343223");
+            
+            
+            Console.WriteLine("Welcome to CRUD App");
+            int id = 0;
+            string name = "";
+            string mobile = "";
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Choose one of the following:");
+                Console.WriteLine("1-Add");
+                Console.WriteLine("2-Edit");
+                Console.WriteLine("3-Delete");
+                Console.WriteLine("4-List");
+                Console.WriteLine("5-Exit Application");
+                
+                switch (Console.ReadLine())
+                {
+                    case "1":                        
+                        Console.WriteLine("Enter Name:");
+                        name = Console.ReadLine();
+                        Console.WriteLine("Enter Mobile:");
+                        mobile = Console.ReadLine();
+                        DatabaseLayer.InsertSingle(name, mobile);
+                        Console.WriteLine("Done, Press any key");
+                        Console.ReadLine();
+                        break;
+                    case "2":                        
+                        Console.WriteLine("Enter Id:");
+                        id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Name:");
+                        name = Console.ReadLine();
+                        Console.WriteLine("Enter Mobile:");
+                        mobile = Console.ReadLine();
+                        DatabaseLayer.EditSingle(id, name, mobile);
+                        Console.WriteLine("Done, Press any key");
+                        Console.ReadLine();
+                        break;
+                    case "3":
+                        Console.WriteLine("Enter Id:");
+                        id = Convert.ToInt32(Console.ReadLine());
+                        DatabaseLayer.DeleteSingle(id);
+                        Console.WriteLine("Done, Press any key");
+                        Console.ReadLine();
+                        break;
+                    case "4":
+                        DatabaseLayer.PrintList();
+                        Console.WriteLine("Done, Press any key");
+                        Console.ReadLine();
+                        break;
+                    case "5":
+                        goto exitapp;                        
+                    default:
+                        goto exitapp;
+                }
+            }
+            exitapp:
+            Console.WriteLine("Thanks for using CRUD App");
+        }
+    }
+}
